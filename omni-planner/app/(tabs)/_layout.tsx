d68d3b3646640c1ea94c01088ad83c0c55442385
rhit-villencr <villencr@rose-hulman.edu>
2024-09-12 15:47:50 -0400

    More visual alterations

--- omni-planner/app/(tabs)/_layout.tsx
+++ omni-planner/app/(tabs)/_layout.tsx
@@ -9,57 +9,71 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <Tabs
+
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        // Dark/Light Theme setup
+        tabBarStyle: {height: 90},
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         tabBarInactiveBackgroundColor: Colors[colorScheme ?? 'light'].background,
         tabBarActiveBackgroundColor: Colors[colorScheme ?? 'light'].tabBarActiveBackgroundColor
       }}>
+
+      {/* Home Tab */}
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: '',
           tabBarIcon: ({ color }) => (
-            <TabBarIcon name={'home'} color={color} />
+            <TabBarIcon name={'home'} color={color} style={[{bottom: -4}]} />
           ),
         }}
       />
+
+      {/* Discorver Tab */}
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: '',
           tabBarIcon: ({ color }) => (
-            <TabBarIcon name={'code-slash'} color={color} />
+            <TabBarIcon name={'code-slash'} color={color} style={[{bottom: -4}]} />
           ),
         }}
       />
+
+      {/* Add Tab */}
       <Tabs.Screen
         name="add"
         options={{
           title: "",
-          tabBarIcon: ({color, focused}) => (
-            <TabBarIcon name={'add-circle-outline'} color={color} size={40} />
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name={'add-circle-outline'} color={color} size={40} style={[{bottom: -4}]}/>
           ),
         }}
       />
+
+      {/* Settings Tab */}
       <Tabs.Screen
         name="settings"
         options={{
-          title: 'Settings',
+          title: '',
           tabBarIcon: ({ color }) => (
-            <TabBarIcon name={'options'} color={color} />
+            <TabBarIcon name={'options'} color={color} style={[{bottom: -4}]} />
           ),
         }}
       />
+
+      {/* Profile Tab */}
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
+          title: '',
           tabBarIcon: ({ color }) => (
-            <TabBarIcon name='person-outline' color={color} />
+            <TabBarIcon name='person-outline' color={color} style={[{bottom: -4}]} />
           ),
         }}
       />
+
     </Tabs>
   );
 }
