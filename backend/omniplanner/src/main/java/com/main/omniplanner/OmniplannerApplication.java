cbc76ef2b140e648f190c93585343918ae7aa131
avichaljadeja2002 <jadejaan@rose-hulman.edu>
2024-09-29 19:59:15 -0400

    adding backend and db

---
+++ backend/omniplanner/src/main/java/com/main/omniplanner/OmniplannerApplication.java
@@ -0,0 +1,13 @@
+package com.main.omniplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniplannerApplication.class, args);
+	}
+
+}
