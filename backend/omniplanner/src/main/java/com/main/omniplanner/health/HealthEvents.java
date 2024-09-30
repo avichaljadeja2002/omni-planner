73b26635bff0597d856ba6592b9b162c15c127d1
rhit-villencr <villencr@rose-hulman.edu>
2024-09-30 17:41:59 -0400

    Mealtracking Stil

--- backend/omniplanner/src/main/java/com/main/omniplanner/health/HealthEvents.java
+++ backend/omniplanner/src/main/java/com/main/omniplanner/health/HealthEvents.java
@@ -1,2 +1,88 @@
-package com.main.omniplanner.health;public class HealthEvents {
+package com.main.omniplanner.health;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.sql.Date;
+import java.sql.Time;
+
+@Entity
+@Table(name = "healthEvents")
+public class HealthEvents {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private int userId;
+
+    private String title;
+
+    private Date event_date;
+
+    private Time event_time;
+
+    private boolean repeating;
+
+    private String repeat_timeline;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return userId;
+    }
+
+    public void setUser_id(int user_id) {
+        this.userId = user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getEvent_date() {
+        return event_date;
+    }
+
+    public void setEvent_date(Date event_date) {
+        this.event_date = event_date;
+    }
+
+    public Time getEvent_time() {
+        return event_time;
+    }
+
+    public void setEvent_time(Time event_time) {
+        this.event_time = event_time;
+    }
+
+    public boolean isRepeating() {
+        return repeating;
+    }
+
+    public void setRepeating(boolean repeating) {
+        this.repeating = repeating;
+    }
+
+    public String getRepeat_timeline() {
+        return repeat_timeline;
+    }
+
+    public void setRepeat_timeline(String repeat_timeline) {
+        this.repeat_timeline = repeat_timeline;
+    }
 }
+
--- backend/omniplanner/src/main/java/com/main/omniplanner/meals/MealEventsRepository.java
+++ backend/omniplanner/src/main/java/com/main/omniplanner/meals/MealEventsRepository.java
@@ -1,2 +1,11 @@
-package com.main.omniplanner.meals;public interface MealEventsRepository {
+package com.main.omniplanner.meals;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MealEventsRepository extends JpaRepository<MealEvents, Integer> {
+    List<MealEvents> findByUserId(int userId);
 }
