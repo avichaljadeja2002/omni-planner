77947fd170b7daadd77e2d761a18f709b4d85126
rhit-villencr <villencr@rose-hulman.edu>
2024-10-09 17:24:31 -0400

    Github CI Testing 19

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,21 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically picks the version you have installed
+      },
+    },
+    rules: {
+      // Disable type declaration checking
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
 ];
