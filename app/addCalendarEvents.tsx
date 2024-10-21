5b1933d190b5e1461e7e21bf00c5e5d9179a7dfb
avichaljadeja2002 <jadejaavichal2020@gmail.com>
2024-10-20 22:23:29 -0400

    getting google calendar working

--- app/addCalendarEvents.tsx
+++ app/addCalendarEvents.tsx
@@ -1,159 +1,55 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
-import { styles } from './styles';
-import { Ionicons } from "@expo/vector-icons";
-import { Dropdown } from 'react-native-element-dropdown';
+import React from 'react'
 import axios from 'axios';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { IPAddr, repeatingData } from './constants';
+import { IPAddr } from './constants';
+import { RootStackParamList } from '../components/Types';
 import { useNavigation } from '@react-navigation/native';
+import GenericAddPageForm from './addEventPage';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '@/components/Types';
-
-export default function AddCalendarEvents() {
-    type AddCalendarTrackerNavigationProp = StackNavigationProp<RootStackParamList, 'addCalendarEvents'>;
-    const navigation = useNavigation<AddCalendarTrackerNavigationProp>();
-
-    const [value, setValue] = useState<string | null>(null);
-
-    const [eventData, setEventData] = useState({
-        user_id: 1,
-        title: '',
-        event_date: new Date(),
-        event_time: new Date(),
-        repeating: false,
-        repeat_timeline: '',
-        description: ''
-    });
 
 
-    const handleChange = (name: string, value: any) => {
-        setEventData({ ...eventData, [name]: value });
-    };
+export default function AddMeals() {
+  type AddMealTrackerNavigationProp = StackNavigationProp<RootStackParamList, 'addMeals'>;
+  const navigation = useNavigation<AddMealTrackerNavigationProp>();
 
-    const handleSave = async () => {
-        const formattedData = {
-            ...eventData,
-            event_date: eventData.event_date.toISOString().split('T')[0],
-            event_time: eventData.event_time.toTimeString().split(' ')[0],
-        };
+  const initialData = {
+    user_id: 1,
+    title: '',
+    event_date: new Date(),
+    event_time: new Date(),
+  };
 
-        try {
-            const response = await axios.post(IPAddr + '/add_calendar_events', formattedData);
-            console.log('Event saved successfully:', response.data);
-        } catch (error) {
-            console.error('Error saving event:', error);
-        }
-        navigation.navigate('calendarEvents')
-    };
+  const fields = [
+    { name: 'title', label: 'Title', type: 'text' },
+    { name: 'event_date', label: 'Date', type: 'date' },
+    { name: 'event_time', label: 'Time', type: 'time' },
+    { name: 'description', label: 'Description', type: 'text' },
+    { name: 'repeating', label: 'Repeating', type: 'dropdown' },
 
-    const handleDateChange = (event: any, selectedDate: any) => {
-        if (selectedDate) {
-            handleChange('event_date', selectedDate);
-        }
-    };
+  ];
 
-    const handleTimeChange = (event: any, selectedTime: any) => {
-        if (selectedTime) {
-            handleChange('event_time', selectedTime);
-        }
-    };
-
-    return (
-        <View style={styles.container}>
-            <View style={{ height: 100 }}></View>
-            <View>
-                <View style={styles.inLine}>
-                    <Text style={styles.sectionHeader}>New Calendar Event</Text>
-                    <TouchableOpacity style={{ marginLeft: 50 }}
-                        onPress={() => console.log("pressed")}
-                        hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}>
-                        <View style={styles.taskicon}>
-                            <Ionicons name="walk-outline" size={30} color={'#000'} />
-                        </View>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.inputContainer}>
-                    <View style={styles.inLine}>
-                        <Text style={styles.inputText}>Title</Text>
-                        <TextInput
-                            style={styles.input}
-                            value={eventData.title}
-                            onChangeText={(text) => handleChange('title', text)}
-                        />
-                    </View>
-
-                    <View style={styles.inLine}>
-                        <Text style={styles.inputText}>Date</Text>
-                        <View style={styles.dateTime}>
-                            <DateTimePicker
-                                value={eventData.event_date}
-                                mode="date"
-                                display="default"
-                                onChange={handleDateChange}
-                            />
-
-                            <DateTimePicker
-                                value={eventData.event_time}
-                                mode="time"
-                                display="default"
-                                onChange={handleTimeChange}
-                            />
-                        </View>
-                    </View>
+  const handleSave = async (saveData: any) => {
+    try {
+      const response = await axios.post(IPAddr + '/add_calendar_events', saveData);
+      console.log('Event saved successfully:', response.data);
+    } catch (error) {
+      console.error('Error saving event:', error);
+    }
+    navigation.navigate('mealTracker')
+  };
 
 
-                    <View style={styles.inLine}>
-                        <Text style={styles.inputText}>Repeating</Text>
-                        <View style={styles.container}>
-                            <Dropdown
-                                style={{ width: 200, borderWidth: 1, padding: 8 }}
-                                data={repeatingData}
-                                maxHeight={300}
-                                labelField="label"
-                                valueField="value"
-                                value={value}
-                                onChange={item => {
-                                    setValue(item.value);
-                                    handleChange('repeat_timeline', item.label);
-                                }}
-                                renderLeftIcon={() => (
-                                    <Ionicons
-                                        name='calendar-outline'
-                                        size={20}
-                                        paddingRight={10}
-                                    />
-                                )}
-                            />
-                        </View>
-                    </View>
+  const handleCancel = (() => {
+    navigation.navigate('calendarEvents')
+  })
 
-                    <View style={styles.inLine}>
-                        <Text style={[styles.inputText, { alignSelf: 'baseline', top: 5 }]}>Description</Text>
-                        <TextInput
-                            style={styles.bigInput}
-                            value={eventData.description}
-                            onChangeText={(text) => handleChange('description', text)}
-                        />
-                    </View>
-                </View>
-            </View>
-            <View style={styles.saveCancelContainer}>
-                <View style={styles.saveCancel}>
-                    <Text style={styles.saveCancelText}>Cancel</Text>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate('calendarEvents')}
-                        hitSlop={{ top: 20, bottom: 20, left: 50, right: 50 }}>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.saveCancel}>
-                    <Text style={styles.saveCancelText}>Save</Text>
-                    <TouchableOpacity
-                        onPress={handleSave}
-                        hitSlop={{ top: 20, bottom: 20, left: 50, right: 50 }}>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </View>
-    );
-}
+  return (
+    <GenericAddPageForm
+      title="New Calendar Event"
+      initialData={initialData}
+      fields={fields}
+      onSave={handleSave}
+      onCancel={handleCancel}
+    />
+  );
+};
+
