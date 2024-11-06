c6ce6746b791dcdb4ef9c3168ea67da7dbf11236
chasevill <chasevill@gmail.com>
2024-11-05 19:17:27 -0500

    Fix Merge Conflict

--- backend/omniplanner/src/test/java/com/main/omniplanner/OmniplannerApplicationTest.java
+++ backend/omniplanner/src/test/java/com/main/omniplanner/OmniplannerApplicationTest.java
@@ -1,19 +1,18 @@
 package com.main.omniplanner;
 
+import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 class OmniplannerApplicationTest {
-
     @Test
     void contextLoads() {
         // This test will pass if the application context loads successfully.
-    }
-
-    @Test
-    void main() {
-        OmniplannerApplication.main(new String[] {});
-        // This will invoke the main method, and it should not throw any exceptions.
     }
 }
