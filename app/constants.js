4281f944bf1a449d823aa6ecd67390f2cfbbb8de
avichaljadeja2002 <jadejaavichal2020@gmail.com>
2024-12-05 16:19:26 -0500

    all good for demo

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,4 @@
-// export const IPAddr = "http://34.204.83.156:8080" // For AWS
+//export const IPAddr = "http://34.204.83.156:8080" // For AWS
 export const IPAddr = "http://127.0.0.1:8080" // For local testing on laptop
 export const logging = true;
 
