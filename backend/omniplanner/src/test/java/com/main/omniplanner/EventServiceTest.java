f4a2ab0c144e562ca637b6c2beb782c1245f5f50
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2024-12-07 18:34:31 -0500

    Attempt to get CI tests to succeed by making changes to EventServiceTest

--- backend/omniplanner/src/test/java/com/main/omniplanner/EventServiceTest.java
+++ backend/omniplanner/src/test/java/com/main/omniplanner/EventServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.*;
 
 public class EventServiceTest {
 
@@ -17,34 +18,34 @@ public class EventServiceTest {
 
     @BeforeEach
     void setUp() {
-        eventService = new eventService();
+        eventService = new EventService();
     }
 
     @Test
     public void testGetSaveEvent() {
         event.setId(0);
         Date date = Date.valueOf("2024-11-05");
-        event.setEvent_date(date);
+        event.setEventDate(date);
         Time time = Time.valueOf("10:30:00");
-        event.setEvent_time(time);
-        event.setMoney(9.99);
-        event.setRepeat_timeline("weekly");
+        event.setEventTime(time);
+        event.setMoney(9.99f);
+        event.setRepeatTimeline("weekly");
         event.setRepeating(true);
         event.setTitle("Team Meeting");
         event.setEvent_type("Work");
-        event.setUser_id(0);
+        event.setUserId(0);
 
         eventService.saveEvent(event);
         List<Event> eventList = eventService.getEventsByUserId(0);
         Event testEvent = eventList.get(0);
         assertEquals(0, testEvent.getId());
-        assertEquals(date, testEvent.getEvent_date());
-        assertEquals(time, testEvent.getEvent_time());
+        assertEquals(date, testEvent.getEventDate());
+        assertEquals(time, testEvent.getEventTime());
         assertEquals(9.99, testEvent.getMoney());
-        assertEquals("weekly", testEvent.getRepeat_timeline());
+        assertEquals("weekly", testEvent.getRepeatTimeline());
         assertEquals(true, testEvent.getRepeating());
         assertEquals("Team Meeting", testEvent.getTitle());
         assertEquals("Work", testEvent.getEvent_type());
-        assertEquals(0, testFinanceEvents.getUser_id());
+        assertEquals(0, testEvent.getUser_id());
     }
 }
