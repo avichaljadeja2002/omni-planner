56117002b81f1c9267d3bd89476619aa814418e7
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2024-12-16 17:54:41 -0500

    Testing notes frontend

--- app/notes.tsx
+++ app/notes.tsx
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import { styles } from './styles';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { cLog } from './log';
+import { IPAddr } from './constants';
+import axios from 'axios';
 
 const NotesApp = () => {
+
+    const initialData = { userId: 1, text: "" }
+
+    const [formData, setFormData] = useState(initialData);
+
+    const handleSave = async () => { 
+        try { 
+            cLog(formData.text); 
+            const hit = IPAddr + '/add_note'; 
+            const response = await axios.post(hit, formData); 
+            cLog('Note saved successfully:' + response.data); 
+        } catch (error) { 
+            console.error('Error saving note'); 
+        } 
+    };
+
+    const handleChange = (name: string, value: any) => {
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const fetchNote = async () => {
+        const hit = IPAddr + '/get_hnote/1';
+        cLog('Fetching note from:' + hit);
+        axios.get(hit)
+            .then(response => {
+                const events = response.data.map((event: any) => ({
+                    text: `${event.text}`,
+                    icon: 'fitness-outline',
+                })).slice(0, 10);;
+                setFormData({ ...formData, text: events[0]?.text || "" });
+            })
+            .catch(error => console.error('Error fetching events:', error));
+    };
+
+    useEffect(() => {
+        fetchNote()
+    }, []);
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.title}>Notes</Text>
                 <TextInput
+                    onChangeText={(text) => handleChange("text", text)}
                     style={styles.textInput}
                     placeholder="Write your notes here..."
                     multiline={true}
                 />
             </View>
             <View style={styles.footer}>
-                <TouchableOpacity style={styles.saveButton}>
+                <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
                     <Text style={styles.saveButtonText}>Save</Text>
                 </TouchableOpacity>
             </View>
