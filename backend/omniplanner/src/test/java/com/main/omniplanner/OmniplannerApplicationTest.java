18912b1131a5a60af0cb6d44e3499da2f1ea02d8
chasevill <98123650+rhit-villencr@users.noreply.github.com>
2025-01-11 15:57:16 -0500

    Trying to fix tests to pass in github CI

--- backend/omniplanner/src/test/java/com/main/omniplanner/OmniplannerApplicationTest.java
+++ backend/omniplanner/src/test/java/com/main/omniplanner/OmniplannerApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.TestPropertySource;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
-@TestPropertySource(properties = "spring.datasource.url=jdbc:sqlite:omniplanner.db")
+@TestPropertySource(locations = "classpath:application-test.properties", properties = "spring.datasource.url=jdbc:sqlite:omniplanner.db")
 class OmniplannerApplicationTest {
 
     @Test
