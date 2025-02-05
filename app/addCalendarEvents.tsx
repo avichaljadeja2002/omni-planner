e7a675f9f8c337b4dfdd94d739dd1faa88e23512
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2025-02-05 14:28:43 -0500

    Initial time now rounded to the nearest 15 minute interval instead of exact time

--- app/addCalendarEvents.tsx
+++ app/addCalendarEvents.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { calendarFields } from '@/constants/constants';
 import GenericAddViewPageForm from './genericAddViewEventPage';
 
+function roundTimeQuarterHour() {
+    var timeToReturn = new Date();
+
+    timeToReturn.setMilliseconds(Math.round(timeToReturn.getMilliseconds() / 1000) * 1000);
+    timeToReturn.setSeconds(Math.round(timeToReturn.getSeconds() / 60) * 60);
+    timeToReturn.setMinutes(Math.round(timeToReturn.getMinutes() / 15) * 15);
+    return timeToReturn;
+}
+
 export default function AddCalendarEvents() {
     const calendarInitialData = {
         title: '',
         event_date: new Date().toISOString().split('T')[0],
-        event_time: new Date().toTimeString().split(' ')[0],
+        event_time: roundTimeQuarterHour().toTimeString().split(' ')[0],
         description: '',
         repeating: false,
         repeat_timeline: '',
