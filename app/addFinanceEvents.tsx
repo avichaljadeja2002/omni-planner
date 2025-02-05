a0d33777f09e8f81506ea0ce25c5eb7b18705a2d
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2025-02-05 14:28:43 -0500

    Initial time now rounded to the nearest 15 minute interval instead of exact time

--- app/addFinanceEvents.tsx
+++ app/addFinanceEvents.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { financeFields } from '@/constants/constants';
 import GenericAddViewPageForm from './genericAddViewEventPage';
 
+function roundTimeQuarterHour() {
+    var timeToReturn = new Date();
+
+    timeToReturn.setMilliseconds(Math.round(timeToReturn.getMilliseconds() / 1000) * 1000);
+    timeToReturn.setSeconds(Math.round(timeToReturn.getSeconds() / 60) * 60);
+    timeToReturn.setMinutes(Math.round(timeToReturn.getMinutes() / 15) * 15);
+    return timeToReturn;
+}
+
 export default function AddFinanceEvents() {
     const financeInitialData = {
         title: '',
         event_date: new Date().toISOString().split('T')[0],
-        event_time: new Date().toTimeString().split(' ')[0],
+        event_time: roundTimeQuarterHour().toTimeString().split(' ')[0],
         repeating: false,
         repeat_timeline: '',
         money: null,
