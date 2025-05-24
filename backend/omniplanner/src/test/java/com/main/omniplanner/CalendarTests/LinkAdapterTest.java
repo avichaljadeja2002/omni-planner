3f905bacaaf700b18b9503262764a5b37c442552
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2025-05-24 07:49:02 -0400

    attempt to fix LinkAdapterTest

--- backend/omniplanner/src/test/java/com/main/omniplanner/CalendarTests/LinkAdapterTest.java
+++ backend/omniplanner/src/test/java/com/main/omniplanner/CalendarTests/LinkAdapterTest.java
@@ -21,16 +21,23 @@ class LinkAdapterTest {
     void testLinkCalendar() {
         String accessToken = "ya29.a0AW4XtxjEhBkK40ZBkqv-TSmgJ-Kmxiyib-9IYmwCjrzH2Mtwu_0Tfnf8p5kXno4bIVa8pAWSr2c2EX_kwzVkHir_v28iwi67EJnnyLjHTpl238qDrk3OUE79CWBveP2VrhIO7ufVKOhG1eaYiA553DvDAG8IGjghEGC7nnI2aCgYKAaQSARUSFQHGX2MimrAisgI1kHK6HmmTZvSNCw0175";
         String token = "4034e083-7065-47d6-bfed-62eb5ba8a06e";
-        // HttpRequest request = HttpRequest.newBuilder()
-        //     .uri(URI.create("http://localhost:8080/link_imap/" + token))
-        //     .header("Authorization", "Bearer " + accessToken)
-        //     .header("Content-Type", "application/json")
-        //     .POST(HttpRequest.BodyPublishers.ofString("{}"))
-        //     .build();
-        CalendarLinkRequest request = new CalendarLinkRequest();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8080/link_imap/" + token))
+            .header("Authorization", "Bearer " + accessToken)
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString("{}"))
+            .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
         assertEquals(
             linkAdapter.linkCalendar(request, token),
-            linkImap.linkImap(request, token)
+            response
         );
     }
 
