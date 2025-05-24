febf8ae7482ef9bdb9da8bddc4b616729e8cffd7
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2025-05-24 09:09:04 -0400

    making changes to LinkAdapter

--- backend/omniplanner/src/main/java/com/main/omniplanner/calendar/LinkAdapter.java
+++ backend/omniplanner/src/main/java/com/main/omniplanner/calendar/LinkAdapter.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LinkAdapter extends LinkGoogleCalendar {
 
     private LinkImap linkImap = new LinkImap();
