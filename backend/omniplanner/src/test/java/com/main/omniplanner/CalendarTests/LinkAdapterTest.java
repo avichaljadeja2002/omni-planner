6fd97def511f7b64b82bb58541a6b5d7f4bb7184
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2025-05-24 07:13:42 -0400

    adding linkAdapter tests

---
+++ backend/omniplanner/src/test/java/com/main/omniplanner/CalendarTests/LinkAdapterTest.java
@@ -0,0 +1,27 @@
+package com.main.omniplanner.CalendarTests;
+
+class LinkAdapterTest {
+    @Autowired
+    private LinkAdapter linkAdapter;
+
+    @Autowired
+    private LinkImap linkImap;
+
+    @Test
+    void testLinkCalendar() {
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:8080/link_imap/" + token))
+            .header("Authorization", "Bearer " + accessToken)
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString("{}"))
+            .build();
+        String token = "4034e083-7065-47d6-bfed-62eb5ba8a06e";
+        assertEquals(linkAdapter.linkCalendar(request, token), linkImap.linkImap(request, token));
+    }
+
+    @Test
+    void testGetGoogleCalendarEvents() {
+        String access_token = "ya29.a0AW4XtxjEhBkK40ZBkqv-TSmgJ-Kmxiyib-9IYmwCjrzH2Mtwu_0Tfnf8p5kXno4bIVa8pAWSr2c2EX_kwzVkHir_v28iwi67EJnnyLjHTpl238qDrk3OUE79CWBveP2VrhIO7ufVKOhG1eaYiA553DvDAG8IGjghEGC7nnI2aCgYKAaQSARUSFQHGX2MimrAisgI1kHK6HmmTZvSNCw0175";
+        assertEquals(linkAdapter.getGoogleCalendarEvents(access_token), linkImap.getImapEvents(access_token));
+    }
+}
