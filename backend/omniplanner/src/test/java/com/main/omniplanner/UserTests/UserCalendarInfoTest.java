d5d918f13065209d223dd050559042d3237e9ceb
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2025-05-24 02:32:12 -0400

    adding testGetSetGoogleCalendarLinked to UserCalendarInfoTest

---
+++ backend/omniplanner/src/test/java/com/main/omniplanner/UserTests/UserCalendarInfoTest.java
@@ -0,0 +1,25 @@
+package com.main.omniplanner.UserTests;
+
+import com.main.omniplanner.user.UserCalendarInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+
+public class UserCalendarInfoTest {
+    private UserCalendarInfo userCalendarInfo;
+
+    @BeforeEach
+    void setUp() {
+        userCalendarInfo = new UserCalendarInfo();
+    }
+
+    @Test
+    public void testGetSetGoogleCalendarLinked(){
+        boolean googleCalendarLinked = true;
+        userCalendarInfo.setGoogleCalendarLinked(googleCalendarLinked);  // Set googleCalendarLinked
+        assertEquals(googleCalendarLinked, userCalendarInfo.isGoogleCalendarLinked());  // Assert that isGoogleCalendarLinked returns the same value
+    }
+}
