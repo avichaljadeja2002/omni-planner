3db783cf19cadff7f4969d6ed77e9d3d66030dba
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2025-05-26 05:02:14 -0400

    Revert "test covering line auditService.logAccountEvent(user.getUsername(), 'Google Account Linked');"
    This reverts commit 01796438be0e3d0a08869f5e6865c66add739a53.

--- backend/omniplanner/src/test/java/com/main/omniplanner/UserTests/UserControllerTest.java
+++ backend/omniplanner/src/test/java/com/main/omniplanner/UserTests/UserControllerTest.java
@@ -216,7 +216,6 @@ public class UserControllerTest {
         Map<String, String> responseBody = (Map<String, String>) response.getBody();
         assertNotNull(responseBody);
         assertTrue(responseBody.get("token").matches("[0-9a-fA-F-]{36}"));
-        verify(auditService).logAccountEvent(user.getUsername(), "Google Account Linked");
     }
 
 
