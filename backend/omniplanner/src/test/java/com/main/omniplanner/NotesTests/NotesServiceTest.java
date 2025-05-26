85f128be902e2584b31656c99bda0193bada0d54
rhit-norflwe <90203262+rhit-norflwe@users.noreply.github.com>
2025-05-26 07:16:36 -0400

    fixing mutation coverage NotesServiceTest

--- backend/omniplanner/src/test/java/com/main/omniplanner/NotesTests/NotesServiceTest.java
+++ backend/omniplanner/src/test/java/com/main/omniplanner/NotesTests/NotesServiceTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NotesServiceTest {
 
